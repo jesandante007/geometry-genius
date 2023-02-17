480e54cbb4840d99a850ea90bfc5fecb66a29e5b
jesandante007 <jesandante007@gmail.com>
2023-02-18 01:02:43 +0600

    added some id & linked the js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const btnTriangle = document.getElementById('btn-triangle');
+const btnRectangle = document.getElementById('btn-rectangle');
+const btnParallelogram  = document.getElementById('btn-parallelogram');
+const btnRhombus = document.getElementById('btn-rhombus');
+const btnPentagon = document.getElementById('btn-pentagon');
+const btnEllipse = document.getElementById('btn-ellipse');
+
+btnTriangle.addEventListener('click', function(e){
+    console.log(e.target)
+})
